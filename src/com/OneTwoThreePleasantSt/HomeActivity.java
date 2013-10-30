82879fe1d16cdcda41ca6c90fc00da6d8db9c0ca
jwolen <jwolen@da64c764-6c14-4ae2-a8e4-862394c20f64>
2013-10-30 03:10:19 +0000

    Basic home screen is now listing text

--- src/com/OneTwoThreePleasantSt/HomeActivity.java
+++ src/com/OneTwoThreePleasantSt/HomeActivity.java
@@ -1,13 +1,39 @@
 package com.OneTwoThreePleasantSt;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
-public class HomeActivity extends Activity 
+public class HomeActivity extends ListActivity 
 {
+    //final ListView weekListView = (ListView) findViewById(R.id.list);
+    static private String[] weekListing =new String[] { "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" };
+
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
-    }
+	
+	//Set The Layout
+	setContentView(R.layout.home_layout);
+
+	setListAdapter (new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, weekListing));
+
+	ListView weeklyListing = getListView();
+
+	/**weekListView.setOnItemClickListener(new OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+		    // When clicked, show a toast with the TextView text
+		    Toast.makeText(getApplicationContext(),
+				   ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+		}
+		});*/
+
+	    }  
 }
+
