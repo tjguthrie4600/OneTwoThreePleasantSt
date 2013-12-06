556f4103d1f5e0cc485842cf29efa20c44721e28
jwolen <jwolen@da64c764-6c14-4ae2-a8e4-862394c20f64>
2013-12-06 05:34:07 +0000

    text based day view is now not breaking!

--- Client/OneTwoThreePleasantSt/src/com/OneTwoThreePleasantSt/CalendarClass.java
+++ Client/OneTwoThreePleasantSt/src/com/OneTwoThreePleasantSt/CalendarClass.java
@@ -1,20 +1,25 @@
 package com.OneTwoThreePleasantSt;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Calendar;
 import java.util.Locale;
+import java.lang.StringBuilder;
 
 public class CalendarClass
 {
     private Calendar cal;
     private int year;
     private int month;
+    private int day;
 
     public CalendarClass()
     {
 	cal = Calendar.getInstance();
 	year = cal.get(cal.YEAR);
 	month = month = cal.get(Calendar.MONTH);
+	day = cal.get(cal.DAY_OF_MONTH);
     }
 
     public String getMonth()
@@ -48,7 +53,7 @@ public class CalendarClass
     {
 	cal.add(Calendar.MONTH, -1);
 	year = cal.get(cal.YEAR);
-	month = month = cal.get(Calendar.MONTH);
+	month = cal.get(Calendar.MONTH);
     }
 
 
@@ -79,4 +84,71 @@ public class CalendarClass
         return 0;
     }
     
+    public int getCurrentDay(){
+	return day;
+    }
+    
+    public int getNextDay(){
+	cal.add(Calendar.DAY_OF_MONTH, 1);
+	int nextDay = cal.get(Calendar.DAY_OF_MONTH);
+	cal.add(Calendar.DAY_OF_MONTH, -1);
+	return nextDay;
+    }
+    
+    public Calendar getCalendar(){
+	return cal;
+    }
+    
+    public String[] createWeek(){
+	Calendar tempcal = Calendar.getInstance();
+        String[] daweek = new String [7];               //eventually this will be another object                                                             
+	
+        String wString = "";
+
+        for (int i=0; i < 7; i++){
+	    wString = tempcal.get(Calendar.MONTH) +"/"+ tempcal.get(Calendar.DAY_OF_MONTH) +"\t\t" + nameOfWeek(tempcal.get(Calendar.DAY_OF_WEEK)) ;
+	    tempcal.add(Calendar.DAY_OF_MONTH, 1);
+	    daweek [i] = wString;
+        }
+
+	String [] bands;// = new String [7];
+	bands = new String [] { "The Cassingles, Jordan Poss, Captain Catfeesh, MORE TBA", "Keep, False Pterodactyl, fuckyourbirthday, Clean Dirty Clean", "Leviathus", "", "Open Mic", "Jucifer, Ancient Shores", "Pigeons Playing Ping Pong" };
+	//This is to fill in the additional information for displaying
+	
+	for (int i = 0; i < 7; i++){
+	    daweek [i] = daweek[i] +"\n" + bands [i];
+	}
+
+        return daweek;
+    }
+    
+    public String nameOfWeek(int dayOfWeek){
+	String name="";
+	switch (dayOfWeek) {
+	case Calendar.SUNDAY:
+	    name = "Sun"; break;
+	case Calendar.MONDAY:
+	    name = "Mon"; break;
+	case Calendar.TUESDAY:
+	    name = "Tue"; break;
+	case Calendar.WEDNESDAY:
+	    name = "Wed"; break;
+	case Calendar.THURSDAY:
+	    name = "Thur"; break;
+	case Calendar.FRIDAY:
+	    name = "Fri"; break;
+	case Calendar.SATURDAY:
+	    name = "Sat"; break;
+	}
+	return name;
+    }
+    
+    public String weekToString (String [] weekString){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < weekString.length; i++ ){
+	    sb.append(weekString[i]+"\n");
+	}
+        return sb.toString();
+    }
+
 }
--- Client/OneTwoThreePleasantSt/src/com/OneTwoThreePleasantSt/HomeActivity.java
+++ Client/OneTwoThreePleasantSt/src/com/OneTwoThreePleasantSt/HomeActivity.java
@@ -7,33 +7,73 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import java.util.ArrayList;
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.os.Handler;
+import org.xmlrpc.android.XMLRPCClient;
+import org.xmlrpc.android.XMLRPCException;
+import android.net.ConnectivityManager;
+import android.content.Context;
+import android.net.NetworkInfo;
+import android.os.Message;
+import java.net.URI;
 
-public class HomeActivity extends ListActivity 
+
+public class HomeActivity extends Activity /// implements Runnable
 {
-    //final ListView weekListView = (ListView) findViewById(R.id.list);
-    static private String[] weekListing =new String[] { "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" };
-
+    private ListView lv;
+    
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
 	
-	//Set The Layout
-	setContentView(R.layout.home_layout);
+	lv = (ListView) findViewById(R.id.homeList);
+	CalendarClass cal = new CalendarClass();
+	String [] week = cal.createWeek();
+	//do something like ifPicture() for a day then it will set the picture with the flyer.
+	// if not then it won't display it
+	// some[] week = cal.createWeek();
+	// for each day, check to see if it has a picture then display it
 
-	setListAdapter (new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, weekListing));
+	// set onClickListeners like in CalendarActivity
+	ArrayList<String> arrayList = new ArrayList<String>();
 
-	ListView weeklyListing = getListView();
+	for (int i = 0; i < week.length; i++)
+            arrayList.add(week[i]);
 
-	/**weekListView.setOnItemClickListener(new OnItemClickListener() {
-		public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-		    // When clicked, show a toast with the TextView text
-		    Toast.makeText(getApplicationContext(),
-				   ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+	ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arrayList);
+	lv.setAdapter(arrayAdapter);
+
+	//	ListView weeklyListing = lv.getListView();
+	listen();
+    }
+
+    // Listen for clicks on the list view
+    public void listen()
+    {
+	lv.setOnItemClickListener(new OnItemClickListener() 
+	    {
+		// When an item in the list is selected 
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+		{
+		    // Store the band name
+		    String name = (String) lv.getItemAtPosition(position);
+		    Intent nextScreen = new Intent(getApplicationContext(), DayActivity.class);
+		    nextScreen.putExtra("currentDay", name);
+		    startActivity(nextScreen);
+		    
 		}
-		});*/
+	    });
+    }
 
-	    }  
+    //public void run()
+    //{
+
+    //}
 }
-
