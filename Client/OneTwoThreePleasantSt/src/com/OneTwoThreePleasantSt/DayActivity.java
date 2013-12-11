5cdd0f7dcd8de13210494a55bdf74fa744193757
jwolen <jwolen@da64c764-6c14-4ae2-a8e4-862394c20f64>
2013-12-11 07:06:24 +0000

    day view band goes to the band page

--- Client/OneTwoThreePleasantSt/src/com/OneTwoThreePleasantSt/DayActivity.java
+++ Client/OneTwoThreePleasantSt/src/com/OneTwoThreePleasantSt/DayActivity.java
@@ -15,13 +15,27 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 import android.content.Context;
+import android.widget.AdapterView.OnItemClickListener;
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.os.Handler;
+import org.xmlrpc.android.XMLRPCClient;
+import org.xmlrpc.android.XMLRPCException;
+import android.net.ConnectivityManager;
+import android.content.Context;
+import android.net.NetworkInfo;
+import android.os.Message;
+import java.net.URI;
 
-public class DayActivity extends Activity 
+
+public class DayActivity extends Activity implements Runnable
 {
     private Button getSomeTickets;
     private Button nextDay;
     private Button prevDay;
     
+    private String selectedBand;
+
     // storage for all of the day information
     private String day;
     private String commentsForDay;
@@ -34,6 +48,8 @@ public class DayActivity extends Activity
 
     //    private ListView lv;
     private ListView listOfBandsView;
+    private String serverResult;
+    private ProgressDialog progress;
 
     public void onCreate(Bundle savedInstanceState) 
     {
@@ -124,5 +140,78 @@ public class DayActivity extends Activity
 		}
 	    }
 	    );
+
+	listOfBandsView.setOnItemClickListener(new OnItemClickListener() 
+	    {
+		// When an item in the list is selected 
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+		{		    
+		    selectedBand = (String) listOfBandsView.getItemAtPosition(position);
+		    talkToServer();
+		}});
     }
+    
+
+    public void talkToServer(){
+	// Check For Network Connection                                                                                                                         
+	ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+	
+	// The User Is Not Connected To A Network                                                                                                               
+	if (networkInfo == null)
+	    {
+		// Tell The User They Are Not Connected                                                                                                         
+		Context context = getApplicationContext();
+		CharSequence text = "An Active Network Connection Is Required";
+		int duration = Toast.LENGTH_LONG;
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+	    }
+	// The User Is Connected To The Internet                                                                                                                
+	else
+	    {
+		// Display Messege                                                                                                                              
+		progress = ProgressDialog.show(DayActivity.this, "Please Wait", "Retrieving Data", true, false);
+		
+		// Create New Thread                                                                                                                            
+		Thread thread = new Thread(DayActivity.this);
+		thread.start();
+	    }
+    }
+
+        // When new thread is run                                                                                                                                               
+    public void run()
+    {
+	// Create The XMLRPC Client                                                                                                                                         
+        URI uri = URI.create("http://98.236.199.243/lamp/pleasant/XML-RPC-Server.py");
+        XMLRPCClient client = new XMLRPCClient(uri);
+        try
+	{
+	    // Make The Call To The Server                                                                                                                                  
+	    serverResult = (String) client.call("PleasantMobile", "2", selectedBand);
+	}
+        catch (XMLRPCException e)
+	{
+	    // Log Errors                                                                                                                                                   
+	    Log.w("XMLRPC Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "Error", e);
+	}
+
+        // Create And Send Message To User Interface Thread                                                                                                                 
+        handler.sendEmptyMessage(0);
+    }
+
+    // Create The Handler To Communicate Back To The User Interface Thread
+    private Handler handler = new Handler()
+	{
+	    @Override
+	    public void handleMessage(Message msg)
+	    {
+		// Get Rid Of The Loading Message                                                                                                                               
+		progress.dismiss();
+		// Start The New Activity, Send It The Result                                                                                                                   
+		Intent nextScreen = new Intent(getApplicationContext(), BandActivity.class);
+		nextScreen.putExtra("result", serverResult);
+		startActivity(nextScreen);
+	    }
+	};
 }
